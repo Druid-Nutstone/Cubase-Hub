143e38ef140163c5fd97d0d8fd7e47efebb2b3f8
David Nuttall <david.nutstone@gmail.com>
2026-02-13 16:02:10 +0000

    fix album layout

--- Cubase.Hub/Controls/Album/AlbumConfigurationControl.Designer.cs
+++ Cubase.Hub/Controls/Album/AlbumConfigurationControl.Designer.cs
@@ -38,31 +38,37 @@
             AlbumTitle = new Cubase.Hub.Controls.BoundControls.BoundTextBox();
             label3 = new Label();
             label1 = new Label();
+            Holder = new TableLayoutPanel();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            Holder.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // AlbumGenre
             // 
-            AlbumGenre.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            AlbumGenre.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             AlbumGenre.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             AlbumGenre.AutoCompleteSource = AutoCompleteSource.CustomSource;
-            AlbumGenre.Location = new Point(713, 115);
+            AlbumGenre.Location = new Point(26, 92);
             AlbumGenre.Name = "AlbumGenre";
-            AlbumGenre.Size = new Size(255, 27);
+            AlbumGenre.Size = new Size(343, 27);
             AlbumGenre.TabIndex = 21;
             // 
             // AlbumComments
             // 
             AlbumComments.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            AlbumComments.Location = new Point(20, 189);
+            AlbumComments.Location = new Point(25, 182);
             AlbumComments.Name = "AlbumComments";
-            AlbumComments.Size = new Size(948, 27);
+            AlbumComments.Size = new Size(747, 27);
             AlbumComments.TabIndex = 20;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label6.Location = new Point(17, 166);
+            label6.Location = new Point(25, 159);
             label6.Name = "label6";
             label6.Size = new Size(136, 20);
             label6.TabIndex = 19;
@@ -70,10 +76,10 @@
             // 
             // label5
             // 
-            label5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label5.Location = new Point(710, 92);
+            label5.Location = new Point(26, 69);
             label5.Name = "label5";
             label5.Size = new Size(102, 20);
             label5.TabIndex = 18;
@@ -81,7 +87,7 @@
             // 
             // AlbumYear
             // 
-            AlbumYear.Location = new Point(20, 115);
+            AlbumYear.Location = new Point(21, 92);
             AlbumYear.Name = "AlbumYear";
             AlbumYear.Size = new Size(125, 27);
             AlbumYear.TabIndex = 17;
@@ -90,7 +96,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label4.Location = new Point(20, 92);
+            label4.Location = new Point(22, 69);
             label4.Name = "label4";
             label4.Size = new Size(90, 20);
             label4.TabIndex = 16;
@@ -98,25 +104,26 @@
             // 
             // AlbumArtist
             // 
-            AlbumArtist.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            AlbumArtist.Location = new Point(710, 39);
+            AlbumArtist.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            AlbumArtist.Location = new Point(26, 23);
             AlbumArtist.Name = "AlbumArtist";
-            AlbumArtist.Size = new Size(258, 27);
+            AlbumArtist.Size = new Size(343, 27);
             AlbumArtist.TabIndex = 15;
             // 
             // AlbumTitle
             // 
-            AlbumTitle.Location = new Point(20, 38);
+            AlbumTitle.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            AlbumTitle.Location = new Point(21, 23);
             AlbumTitle.Name = "AlbumTitle";
-            AlbumTitle.Size = new Size(409, 27);
+            AlbumTitle.Size = new Size(364, 27);
             AlbumTitle.TabIndex = 14;
             // 
             // label3
             // 
-            label3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label3.Location = new Point(710, 16);
+            label3.Location = new Point(26, -3);
             label3.Name = "label3";
             label3.Size = new Size(49, 20);
             label3.TabIndex = 13;
@@ -126,28 +133,65 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label1.Location = new Point(17, 16);
+            label1.Location = new Point(21, 0);
             label1.Name = "label1";
             label1.Size = new Size(91, 20);
             label1.TabIndex = 12;
             label1.Text = "Album Title";
             // 
+            // Holder
+            // 
+            Holder.ColumnCount = 2;
+            Holder.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            Holder.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            Holder.Controls.Add(panel1, 0, 0);
+            Holder.Controls.Add(panel2, 1, 0);
+            Holder.Dock = DockStyle.Top;
+            Holder.Location = new Point(0, 0);
+            Holder.Name = "Holder";
+            Holder.RowCount = 1;
+            Holder.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            Holder.Size = new Size(798, 145);
+            Holder.TabIndex = 22;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(AlbumTitle);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(AlbumYear);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(393, 139);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(AlbumGenre);
+            panel2.Controls.Add(AlbumArtist);
+            panel2.Controls.Add(label5);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(402, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(393, 139);
+            panel2.TabIndex = 1;
+            // 
             // AlbumConfigurationControl
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(AlbumGenre);
+            Controls.Add(Holder);
             Controls.Add(AlbumComments);
             Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(AlbumYear);
-            Controls.Add(label4);
-            Controls.Add(AlbumArtist);
-            Controls.Add(AlbumTitle);
-            Controls.Add(label3);
-            Controls.Add(label1);
             Name = "AlbumConfigurationControl";
-            Size = new Size(978, 241);
+            Size = new Size(798, 229);
+            Holder.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -164,5 +208,8 @@
         private BoundControls.BoundTextBox AlbumTitle;
         private Label label3;
         private Label label1;
+        private TableLayoutPanel Holder;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
